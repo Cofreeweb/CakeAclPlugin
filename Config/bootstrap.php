6dd2282011d9dcc6eb01016be6d1748a8fb3ed02
javikalsan <jcalzadosanchez@gmail.com>
2013-12-09 16:46:17 +0100

    Comiteando bootstrap y nueva configuración

---
+++ Config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Paste in your bootstrap.php app configuration:
+ * CakePlugin::load( 'Acl', array('routes' => true, 'bootstrap' => true));
+ * to load the acl.php file configuration app/Config/acl.php
+ */
+
+// Load a 'default' Acl configuration for use in the application from app/Config/acl.php
+Configure::load( 'acl');
