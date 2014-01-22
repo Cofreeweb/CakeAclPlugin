cc562615e0097f40bfe72d6caf9e2f39a3449d24
Alfonso Etxeberria <alfonso@Arambi.local>
2014-01-22 09:06:11 +0400

    a

---
+++ Config/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+$config ['SettingConfig'] = array(
+    'User' => array(
+        'registerFlash' => array(
+            'i18n' => true,
+            'default' => '¡Gracias por registrarte! Te hemos enviado un email para verificar tu cuenta.'
+        ),
+        'registerFlashError' => array(
+            'i18n' => true,
+            'default' => 'No ha sido posible realizar el registro correctamente. Por favor, inténtalo de nuevo.'
+        ),
+        'registerSubject' => array(
+            'i18n' => true,
+            'default' => 'Bienvenido a nuestro web'
+        ),
+        'forgotPasswordFlash' => array(
+            'i18n' => true,
+            'default' => 'Por favor, revisa tu correo electrónico.'
+        ),
+        'forgotPasswordFlashError' => array(
+            'i18n' => true,
+            'default' => 'El email no es válido o no está en nuestra base de datos.'
+        ),
+    )
+);
